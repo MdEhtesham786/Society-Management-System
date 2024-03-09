e222727cd97c149ee817b49d09c93fc51cac6982
Ehtesham Shaikh <mdehteshamshaikh1@gmail.com>
2024-03-10 04:36:31 +0530

    slaught database creation

---
+++ backend/models/userModel.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import dotenv from "dotenv";
+dotenv.config();
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, 'Please Enter your Username'],
+        minLength: [4, 'Name should have more than 4 characters']
+    },
+    email: {
+        type: String,
+        required: [true, 'Please Enter your Email'],
+        minLength: [4, 'Name should have more than 4 characters']
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false
+    },
+    avatar: {
+        publicID: {
+            type: String
+        },
+
+        url: {
+            type: String
+        }
+    },
+    roles: {
+        type: Array,
+        default: ['manager']
+    }
+});
+// Hashing password before save
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        next();
+    }
+    this.password = await bcrypt.hash(this.password, 10);
+});
+
+//Compare password 
+userSchema.methods.comparePassword = async function (enteredPassword) {
+    return bcrypt.compare(enteredPassword, this.password);
+};
+const userModel = mongoose.model('user', userSchema);
+export { userModel };
---
+++ backend/server.js
@@ -0,0 +1,35 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./db/connect.js";
+dotenv.config();
+const port = process.env.PORT || 5000;
+const hostname = '127.0.0.1';
+const app = express();
+import cors from "cors";
+app.use(cors());
+import { userModel } from "./models/userModel.js";
+app.get('/', async (req, res) => {
+    try {
+        res.json({
+            success: true,
+            name: "Ehtesham"
+        });
+    } catch (err) {
+        console.log(err);
+    }
+});
+const start = async (url) => {
+    connectDB(url);
+    app.listen(port, () => {
+        console.log(`This server is running on port http://${hostname}:${port}`);
+    });
+    process.on("unhandledRejection", err => {
+        console.log(`Error: ${err.message}`);
+        console.log('Shutting down the server due to Unhandled Promise Rejection');
+        server.close(() => {
+            process.exit(1);
+        });
+    });
+
+};
+start(process.env.MONGO_URI);
