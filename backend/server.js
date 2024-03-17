4d57c81b01988b5bc4f02c920c85f8e9c9867966
Ehtesham Shaikh <mdehteshamshaikh1@gmail.com>
2024-03-17 20:47:47 +0530

    slaught

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const hostname = '127.0.0.1';
 const app = express();
 import cors from "cors";
 app.use(cors());
-import { userModel } from "./models/userModel.js";
+import userModel from "./models/userModel.js";
 app.get('/', async (req, res) => {
     try {
         res.json({
